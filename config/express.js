60caeab69ab500ff3a2243350098bfc0fc8d6375
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-04 18:56:16 +1300

    express init

--- config/express.js
+++ config/express.js
@@ -1,1 +1,10 @@
 const express = require('express');
+
+module.exports = function () {
+    // INITIALISE EXPRESS //
+    const app = express();
+    app.use((req, res, next) => {
+        console.log(`##### ${req.method} ${req.path} #####`);
+        next();
+    });
+}
