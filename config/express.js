be9a6e1509d3e1cc052ac75ba762ca92f34e53a4
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-04 19:34:57 +1300

    allow access through express

--- config/express.js
+++ config/express.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const allowCrossOriginRequests = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    next();
+};
 
 module.exports = function () {
     // INITIALISE EXPRESS //
     const app = express();
+    app.use(allowCrossOriginRequests);
     app.use((req, res, next) => {
         console.log(`##### ${req.method} ${req.path} #####`);
         next();
-    });
-}
+    })
+    app.rootUrl = '/api/v1';
+    return app;
+};
