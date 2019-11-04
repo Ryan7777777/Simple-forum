85887fe7c1c1490e098a5ef96d29c0ebff30865c
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-04 18:55:53 +1300

    main init and package update

---
+++ main.js
@@ -0,0 +1,23 @@
+const db = require('./config/db');
+const express = require('./config/express');
+
+const app = express();
+const port = 3668;
+
+// Test connection to MySQL on start-up
+async function testDbConnection() {
+    try {
+        await db.createPool();
+        await db.getPool().getConnection();
+    } catch (err) {
+        console.error(`Unable to connect to MySQL: ${err.message}`);
+        process.exit(1);
+    }
+}
+
+testDbConnection()
+    .then(function () {
+        app.listen(port, function () {
+            console.log(`Listening on port: ${port}`);
+        });
+    });
