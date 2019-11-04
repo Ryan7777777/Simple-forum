7d77fcd8d24a927bfa46d1c06be2c024f8e4ac3f
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-04 19:34:29 +1300

    not port number needed

--- config/db.js
+++ config/db.js
@@ -5,10 +5,10 @@ let pool = null;
 exports.createPool = async function () {
     pool = mysql.createPool({
         multipleStatements: true,
-        host:'127.0.0.1:3306',
+        host:'127.0.0.1',
         user:'ryan',
         password: 'Nemo0422003',
-        database:'client'
+        database:'forum_db'
     });
 };
 
