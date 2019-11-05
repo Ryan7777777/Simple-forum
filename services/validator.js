bef35a038d377cdb5769b598f34a73686b98fc27
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-05 16:24:15 +1300

    validator init

---
+++ services/validator.js
@@ -0,0 +1,25 @@
+const Ajv = require('ajv');
+const ajv = new Ajv({removeAddition:'all'});
+const swaggerApi = require('../resources/api_spec');
+
+ajv.addSchema(swaggerApi, 'swagger');
+ajv.addFormat('password', /.*/);
+ajv.addFormat('binary', /.*/);
+
+exports.checkAgainstSchema = function (schemaPath, data, requireNotEmpty = true) {
+    const schemaRef = 'swagger#/' + schemaPath;
+    try {
+        if (ajv.validate({ $ref: schemaRef }, data)) {
+            if (requireNotEmpty && Object.keys(data).length === 0) {
+                return 'no valid fields provided';
+            }
+            else {
+                return true;
+            }
+        } else {
+            return ajv.errorsText();
+        }
+    } catch (err) {
+        return err.message;
+    }
+};
