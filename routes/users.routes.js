ed4609b338123e572b21873286bdc868300adb76
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-06 12:05:18 +1300

    user logout route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -7,11 +7,6 @@ module.exports = function (app) {
 
     app.route(app.rootUrl + '/users/login')
         .post(users.login);
-
     app.route(app.rootUrl + '/users/logout')
-        .post(authenticate.loginRequired, users.logout);
-
-    app.route(app.rootUrl + '/users/:id')
-        .get(authenticate.setAuthenticatedUser, users.view)
-        .patch(authenticate.loginRequired, users.change);
+        .post(authenticate.loginRequired,users.logout)
 };
