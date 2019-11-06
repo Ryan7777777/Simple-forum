bcddb03f63edd9684544ab3b17265cddc35e4e35
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-06 18:03:29 +1300

    post controller init create new post in controller(validate)

---
+++ controllers/post.controller.js
@@ -0,0 +1,27 @@
+const Post = require('../model/post .model');
+const validator = require('../services/validator');
+
+exports.create = async function(req,res){
+    const id = req.authenticatedId;
+    let validation = validator.checkAgainstSchema(
+        'Post',
+        req.body
+    );
+    if (validation !== true){
+        res.statusMessage = 'Bad request: ${validation}';
+        res.status(400)
+            .send();
+    }  else{
+        try{
+           await Post.new_post(req.body.title,req.body.content,id)
+            res.statusMessage = 'Create';
+            res.status(201)
+             .send();
+        } catch(err){
+            console.log(err);
+            res.statuesMessage = 'Internal Server Error';
+            res.status(500)
+                .send();
+        }
+    }
+};
