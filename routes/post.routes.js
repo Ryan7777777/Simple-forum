ec66ac2adf5663b9b83e7f1adf56843ec0bc3e4d
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-07 14:04:12 +1300

    edit post route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,4 +4,6 @@ const authenticate = require('../middleware/authenticate');
 module.exports = function (app){
     app.route(app.rootUrl+'/newpost')
         .post(authenticate.loginRequired,post.create);
+    app.route (app.rootUrl+'/editpost/:id')
+        .patch(authenticate.loginRequired,post.edit);
 }
