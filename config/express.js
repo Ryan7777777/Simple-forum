4cc63ac7d27fdb010b0d4cb38d5d0c7b8cf75222
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-10 18:26:14 +1300

    allow dynamicBody parser on express

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const allowCrossOriginRequests = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authorization');
@@ -7,11 +8,35 @@ const allowCrossOriginRequests = function (req, res, next) {
     next();
 };
 
+const jsonParser = bodyParser.json();
+const upload = multer({ limits: { fileSize: 20e6 } });
+const multipartParser = upload.single('photo');  // 20 MB
 
+function rawParser (req, res, next) {
+    let data = new Buffer('');
+    req.on('data', function (chunk) {
+        data = Buffer.concat([data, chunk]);
+    });
+    req.on('end', function () {
+        req.body = data;
+        next();
+    });
+};
+
+function dynamicBodyParser(req, res, next) {
+    const contentType = req.header('Content-Type') || '';
+    if (contentType === 'image/jpeg' || contentType === 'image/png' || contentType === 'text/plain') {
+        rawParser(req, res, next);
+    } else if (contentType.startsWith('multipart/form-data')) {
+        multipartParser(req, res, next);
+    } else {
+        jsonParser(req, res, next);
+    }
+}
 module.exports = function () {
     // INITIALISE EXPRESS //
     const app = express();
-    app.use(bodyParser.json());
+    app.use(dynamicBodyParser);
     app.use(allowCrossOriginRequests);
     app.use((req, res, next) => {
         console.log(`##### ${req.method} ${req.path} #####`);
