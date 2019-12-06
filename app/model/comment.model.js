8768cbc8364654d33608463c2822a04126f625e1
Ryan7777777 <ryanleung7777@gmail.com>
2019-12-06 15:22:18 +1300

    save all comment on the comment table including the first comment for the post

--- app/model/comment.model.js
+++ app/model/comment.model.js
@@ -2,10 +2,10 @@ const db = require('../../config/db');
 const errors = require('../services/errors');
 
 exports.new_comment = async function(Userid,content,Postid){
-    const newpost_sql = "INSERT INTO COMMENT(related_user, related_post, comment_content,lastupdate) VALUES (?, ? ,?,?)";
+    const newcomment_sql = "INSERT INTO COMMENT(related_user, related_post, comment_content,lastupdate) VALUES (?, ? ,?,?)";
     const updatetime_sql = "UPDATE POST Set last_update = ? WHERE post_id = ?";
     try{
-        await db.getPool().query(newpost_sql,[Userid,Postid,content,new Date()]);
+        await db.getPool().query(newcomment_sql,[Userid,Postid,content,new Date()]);
         await db.getPool().query(updatetime_sql,[new Date(),Postid]);
     } catch(err){
         errors.logSqlError(err);
--- app/model/post.model.js
+++ app/model/post.model.js
@@ -1,16 +1,16 @@
 const db = require('../../config/db');
 const errors = require('../services/errors');
-
 exports.new_post = async function(title,content,id){
-    const new_post_query = 'INSERT INTO Post (user, post_title, post_content, last_update) VALUES (?, ?, ?, ?)';
+    const new_post_query = 'INSERT INTO Post (user, post_title, last_update) VALUES (?, ?, ?)';
+    const newcomment_query = "INSERT INTO COMMENT(related_user, related_post, comment_content,lastupdate,top_comment) VALUES (?, ? ,?,?,?)";
     const newpost_data =[
         id,
         title,
-        content,
         new Date()
     ];
     try{
-        db.getPool().query(new_post_query,newpost_data)
+        const rows = await db.getPool().query(new_post_query,newpost_data);
+        await db.getPool().query(newcomment_query,[id,rows.insertId,content,new Date(),1]);
     } catch(err){
         errors.logSqlError(err);
         throw err;
@@ -31,9 +31,12 @@ exports.author_checker = async function (PostId,UserId) {
     }
 };
 exports.edit_post = async function(PostId,title,content){
-    const post_edit_query = "UPDATE Post Set post_title = ?,post_content = ?,last_update = ? WHERE post_id = ?";
+    const post_edit_query = "UPDATE Post Set post_title = ?,last_update = ? WHERE post_id = ?";
+    const comment_edit_query = "UPDATE Comment Set comment_content = ?,lastupdate = ? WHERE related_post = ? AND top_comment = ?";
+    console.log(PostId)
     try{
-        db.getPool().query(post_edit_query,[title,content,new Date(),PostId]);
+        db.getPool().query(post_edit_query,[title,new Date(),PostId]);
+        db.getPool().query(comment_edit_query,[content,new Date(),PostId,1]);
 
     } catch(err){
         errors.logSqlError(err);
@@ -41,13 +44,13 @@ exports.edit_post = async function(PostId,title,content){
     }
 };
 exports.allpost = async function(){
-    const get_all_query = "SELECT post_title, post_content, last_update, user_name FROM Post JOIN User ON user = user_id ORDER BY last_update DESC";
+    const get_all_query = "SELECT post_title, last_update, user_name FROM Post JOIN User ON user = user_id ORDER BY last_update DESC";
     try{
       const rows = await db.getPool().query(get_all_query);
       return rows.map(row => ({
             'title': row.post_title,
             'content': row.post_content,
-            'date': row.time_posted,
+            'date': row.last_update,
             'author': row.user_name
             }));
     } catch(err){
